2f9238d98d060102f61d52ecf2a7c3cb0d9a0c66
helifazdev <helifazsilva@gmail.com>
2021-09-16 23:09:18 -0300

    Incompleto

--- src/Exercícios_01/atividade_6.java
+++ src/Exercícios_01/atividade_6.java
@@ -3,6 +3,11 @@ package Exercícios_01;
 //Curso Java Completo (Udemy) | Prof. Dr. Nelio Alves
 //Exercíco sobre Estrututra condicional(if-else)
 
-//
+//Você deve fazer um programa que leia um valor quaquer e apresente uma mensagem dizendo em qual dos seguites
+//intervalos ({0,25}, {25,50}, {50,75}, {75,100}) este valor se encontra. Obviamente se o valor não estiver em
+//nenhum destes intervalos, deverá ser impressa a mensagem "Fora do intervalo"
 public class atividade_6 {
+    public static void main(String[] args) {
+
+    }
 }
--- src/Exercícios_01/atividade_7.java
+++ src/Exercícios_01/atividade_7.java
@@ -3,6 +3,9 @@ package Exercícios_01;
 //Curso Java Completo (Udemy) | Prof. Dr. Nelio Alves
 //Exercíco sobre Estrututra condicional(if-else)
 
-//
+//Leia 2 valores com uma casa decimal (x,y), que devem representar as coordenadas de um ponto em um plano. A seguir,
+//determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x=y=0).
+//se o ponto estiver na origem,escreva a mensagem "Origem"
+//Se o ponto estiver sobre um dos eixos escreva "Eixo X" ou "Eixo Y", Comforme for a situação
 public class atividade_7 {
 }
