08beb2c0a4ddde709ec18eee6280ace525b93ccd
helifazdev <helifazsilva@gmail.com>
2021-09-23 21:33:13 -0300

    completo

--- src/Exercícios_02/atividade_5.java
+++ src/Exercícios_02/atividade_5.java
@@ -1,7 +1,30 @@
 package Exercícios_02;
+import java.util.Scanner;
 //Curso Java Completo (Udemy) | Prof. Dr. Nelio Alves
 //Exercícios sobre estrutura Sequencial (entrada, processamento, saída)
 
-//
+//Fazer um programa para ler o código de uma peça 1, o número da peça1, o valor unitário de cada peça 1, o código de uma
+//peça 2, o numero da de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago
 public class atividade_5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite o código da primeira peça: ");
+        int codpeça1 = input.nextInt();
+        System.out.print("Digite o número de peças: ");
+        int numpeça1 = input.nextInt();
+        System.out.print("Digite o valor da peça: ");
+        double valpeça1 = input.nextDouble();
+        System.out.print("Digite o código da Segunda peça: ");
+        int codpeça2 = input.nextInt();
+        System.out.print("Digite número de peças: ");
+        int numpeça2 = input.nextInt();
+        System.out.print("Digite o valor da peça: ");
+        double valpeça2 = input.nextDouble();
+
+        double prod1 = (numpeça1 * valpeça1);
+        double prod2 = (numpeça2 * valpeça2);
+        double total = prod1 + prod2;
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f",total);
+    }
 }
--- src/Exercícios_02/atividade_6.java
+++ src/Exercícios_02/atividade_6.java
@@ -1,6 +1,32 @@
 package Exercícios_02;
+import java.util.Scanner;
 //Curso Java Completo (Udemy) | Prof. Dr. Nelio Alves
 //Exercícios sobre estrutura Sequencial (entrada, processamento, saída)
 
+//Fazer um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e
+//mostre:
+//a)area do triângulo retângulo que tem A por base e C por altura
+//b)a área do circulo de raio C. (pi = 3.14159).
+//c)a area de trapézio que tem A e B por bases e C por altura.
+//d)a área de quadrado que tem lado B.
+//a area do retangulo que tem lados A e B.
 public class atividade_6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double pi = 3.14159;
+        System.out.print("Digite o valor 'A': ");
+        double A = input.nextDouble();
+        System.out.print("Digite o valor 'B': ");
+        double B = input.nextDouble();
+        System.out.print("Digite o valor 'C': ");
+        double C = input.nextDouble();
+
+        double tri = A * C / 2.0;
+        double cir = pi * (C * C);
+        double tra = ((A + B) * C / 2.0);
+        double qua = B * B;
+        double ret = A * B;
+
+        System.out.printf("TRIANGULO: %.3f\nCIRCULO: %.3f\nTRAPEZIO: %.3f\nQUADRADO: %.3f\nRETANGULO: %.3f",tri,cir,tra,qua,ret);
+    }
 }
